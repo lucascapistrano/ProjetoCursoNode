139aabc92075cd47a68dd697853cf3dd1b35fb72
lucascapistrano <capistrano.lucas@hotmail.com>
2016-10-31 10:03:07 -0300

    Adicionar o EJS. Adicionar a pasta config para que as configurações do servidor estejam no arquivo server.

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
-var express = require('express');
-var app = express();
+var app = require('./config/server');
 
-app.listen(3000);
+app.get('/', function(req, res) {
+	res.render("home/index");
+});
+
+app.get('/formulario_inclusao_noticia', function(req, res){
+	res.render("admin/form_add_noticia");
+});
+
+app.get('/noticias', function(req, res){
+	res.render("noticias/noticias");
+});
+
+app.listen(3000, function() {
+	console.log("Servidor ON");
+});
